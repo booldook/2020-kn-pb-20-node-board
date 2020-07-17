a1b97a68aa2681ad51bdab8053d2dac6c27930db
booldook <booldook@gmail.com>
2020-07-17 12:50:00 +0900

    gbook.js

--- public/js/gbook.js
+++ public/js/gbook.js
@@ -1,1 +1,14 @@
 // uglifyjs ./public/js/gbook.js -c -m -o ./public/js/gbook.min.js.min.js
+function onGbookSubmit(f) {
+	if(f.writer.value.trim() == "") {
+		alert('작성자란이 비어있습니다.');
+		f.writer.focus();
+		return false;
+	}
+	if(f.comment.value.trim() == "") {
+		alert('한줄 내용이 비어있습니다.');
+		f.comment.focus();
+		return false;
+	}
+	return true;
+}
