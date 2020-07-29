b92a0b23abad4feb21eea7c6bc8ef07f781c5754
booldook <booldook@gmail.com>
2020-07-29 13:10:04 +0900

    multer 작업중

--- upload.js
+++ upload.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const multer  = require('multer');
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, '/storage'));
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	}
+});
+ 
+const upload = multer({ storage: storage })
 
 app.listen(3001, () => { console.log('http://127.0.0.1:3001'); });
 
-app.use("/", express.static(path.joins(__dirname, './public')));
-app.use("/uploads", express.static(path.joins(__dirname, './storage')));
+app.use("/", express.static(path.join(__dirname, './public')));
+app.use("/uploads", express.static(path.join(__dirname, './storage')));
+
+app.post('/save', upload.single('upfile'), (req, res, next) => {
+	res.send("저장");
+});
