01896a01f988ba967cbc3a6afc496226a6571734
booldook <booldook@gmail.com>
2020-07-31 11:28:33 +0900

    gallery-wr 완성

---
+++ public/js/gallery.js
@@ -0,0 +1,15 @@
+function onGallerySubmit(f) {
+	var title = f.title.value.trim();
+	var writer = f.writer.value.trim();
+	if(title == '') {
+		alert("제목을 작성해 주세요.");
+		f.title.focus();
+		return false;
+	}
+	if(writer == '') {
+		alert("작성자를 기재해 주세요.");
+		f.writer.focus();
+		return false;
+	}
+	return true;
+}
--- router/gallery-router.js
+++ router/gallery-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pug = {headTitle: "Node/Express 갤러리", css: "gallery", js: "gallery"};
+const { pool } = require('../modules/mysql-conn');
 const { upload } = require('../modules/multer-conn');
 
 router.get(['/', '/list', '/list/:page'], (req, res, next) => {
@@ -24,12 +25,32 @@ router.get('/downlaod/:file', (req, res, next) => {
 	res.send("다운로드");
 });
 
-router.post('/save', upload.array('upfile'), (req, res, next) => {
+router.post('/save', upload.array('upfile'), async (req, res, next) => {
+	let sql, sqlVal = [], connect, result;
 	if(req.banExt) {
 		res.send(`<script>alert('${req.banExt} 타입은 업로드 할 수 없습니다.')</script>`);
 	}
 	else {
-		res.send(req.files);
+		try {
+			sqlVal[0] = req.body.title;
+			sqlVal[1] = req.body.writer;
+			sqlVal[2] = req.body.content;
+			sql = 'INSERT INTO gallery SET title=?, writer=?, content=?';
+			for(let i in req.files) {
+				if(i == 0) sql += ', realfile=?, savefile=?';
+				else sql += ', realfile'+(Number(i)+1)+'=?, savefile'+(Number(i)+1)+'=?';
+				sqlVal.push(req.files[i].originalname);
+				sqlVal.push(req.files[i].filename);
+			}
+			const connect = await pool.getConnection();
+			const result = await connect.execute(sql, sqlVal);
+			connect.release();
+			res.json(result[0]);
+		}
+		catch(e) {
+			console.log(e);
+			next(e);
+		}
 	}
 });
 
