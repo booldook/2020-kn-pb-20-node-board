da80becddeea354b947cd0615eaa0ef630951f22
booldook <booldook@gmail.com>
2020-07-31 10:51:52 +0900

    upload.array

--- router/gallery-router.js
+++ router/gallery-router.js
@@ -24,9 +24,13 @@ router.get('/downlaod/:file', (req, res, next) => {
 	res.send("다운로드");
 });
 
-router.post('/save', upload.single('upfile'), (req, res, next) => {
-	if(req.banExt) res.send(`<script>alert('${req.banExt} 타입은 업로드 할 수 없습니다.')</script>`)
-	else res.send("저장");
+router.post('/save', upload.array('upfile'), (req, res, next) => {
+	if(req.banExt) {
+		res.send(`<script>alert('${req.banExt} 타입은 업로드 할 수 없습니다.')</script>`);
+	}
+	else {
+		res.send(req.files);
+	}
 });
 
 module.exports = router;
