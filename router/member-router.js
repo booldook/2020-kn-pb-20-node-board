0287efa48f483028d087846580d1a5123c82063c
booldook <booldook@gmail.com>
2020-08-07 11:21:34 +0900

    회원가입 폼 완성

--- router/member-router.js
+++ router/member-router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const pug = { headTitle: "Node/Express 회원관리", css: "member", js: "member" };
 
 router.get('/join', (req, res, next) => {
+	pug.title = "회원가입";
 	res.render('member/member-join.pug', pug);
 });
 
