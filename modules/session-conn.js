876a4fe39b65039c2ddd620abed9c4bb57bf4c18
booldook <booldook@gmail.com>
2020-08-11 12:18:50 +0900

    session-conn

--- modules/session-conn.js
+++ modules/session-conn.js
@@ -1,6 +1,19 @@
 const expressSession = require('express-session');
+const MySQLStore = require('express-mysql-session')(expressSession);
+ 
+const options = {
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME
+};
+ 
+const sessionStore = new MySQLStore(options);
 
 const session = expressSession({
+	key: 'booldook-cookie',
+	store: sessionStore,
 	secret: process.env.SESSION_SALT,
 	resave: false,
 	saveUninitialized: true,
