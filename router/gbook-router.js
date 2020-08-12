33a1b9da70ae81dac6063330b1e14d2ea4c6f844
booldook <booldook@gmail.com>
2020-08-12 10:59:19 +0900

    gbook-router

--- router/gbook-router.js
+++ router/gbook-router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { pool, mysqlErr } = require('../modules/mysql-conn');
+const { pool, mysqlErr, queryExecute } = require('../modules/mysql-conn');
 const moment = require('moment');
 const pagerInit = require('../modules/pager-conn');
+let sql, sqlVal, result, pager;
 
 // 127.0.0.1:3000/gbook
 // 127.0.0.1:3000/gbook?cnt=10
@@ -12,16 +13,12 @@ const pagerInit = require('../modules/pager-conn');
 // 127.0.0.1:3000/gbook/list/3?cnt=10
 // console.log(page, cnt, stRec);
 router.get(['/', '/list', '/list/:page'], async (req, res, next) => {
-	let connect, sql, sqlVal, result, pager;
 	try {
 		pager = await pagerInit(req, '/gbook/list', 'gbook');
-		connect = await pool.getConnection();
 		sql = 'SELECT * FROM gbook ORDER BY id DESC LIMIT ?, ?';
-		sqlVal = [pager.stRec, pager.cnt];
-		result = await connect.execute(sql, sqlVal);
-		connect.release();
-		for(let v of result[0]) v.createdAt = moment(v.createdAt).format('YYYY-MM-DD hh:mm:ss');
-		const pug = { css: 'gbook', js: 'gbook', lists: result[0], pager };
+		result = await queryExecute(sql, [pager.stRec, pager.cnt]);
+		for(let v of result) v.createdAt = moment(v.createdAt).format('YYYY-MM-DD hh:mm:ss');
+		const pug = { css: 'gbook', js: 'gbook', lists: result, pager };
 		res.render('gbook/gbook.pug', pug);
 	}
 	catch(e) {
@@ -30,18 +27,15 @@ router.get(['/', '/list', '/list/:page'], async (req, res, next) => {
 });
 
 router.post('/save', async (req, res, next) => {
-	let connect, sql, sqlVal, result;
 	let { writer, comment } = req.body;
-	try {
-		sql = 'INSERT INTO gbook SET writer=?, comment=?';
-		sqlVal = [writer, comment];
-		connect = await pool.getConnection();
-		result = await connect.execute(sql, sqlVal);
-		res.redirect('/gbook');
-	}
-	catch(e) {
-		next(mysqlErr(e));
-	}
+	sql = 'INSERT INTO gbook SET writer=?, comment=?';
+	result = await queryExecute(sql, [writer, comment]);
+	res.redirect('/gbook');
+});
+
+router.get('/rev/:id', async (req, res, next) => {
+	let id = req.params.id;
+
 });
 
 module.exports = router;
